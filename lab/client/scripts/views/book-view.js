8e12ea0b19f99aebc405777cbfa4b9e8caf38d4d
Jeremy Crawford <jfcraw08@gmail.com>
2018-03-04 21:05:04 -0800

    all comments complete (i think)

--- lab/client/scripts/views/book-view.js
+++ lab/client/scripts/views/book-view.js
@@ -82,14 +82,17 @@ var app = app || {};
   };
 
 // COMMENT: What is the purpose of this method?
+// Purpose is to initialize the search form page when called, then provide data for the search results page
   bookView.initSearchFormPage = function() {
     resetView();
     $('.search-view').show();
     $('#search-form').on('submit', function(event) {
       // COMMENT: What default behavior is being prevented here?
+      //Default behavior being prevented here is page refresh.
       event.preventDefault();
 
       // COMMENT: What is the event.target, below? What will happen if the user does not provide the information needed for the title, author, or isbn properties?
+      //Event target is the form field that has been filled out by the user. If the user leaves a value blank, the form will set the value to blank and execute the search.
       let book = {
         title: event.target.title.value || '',
         author: event.target.author.value || '',
@@ -99,6 +102,7 @@ var app = app || {};
       module.Book.find(book, bookView.initSearchResultsPage);
 
       // COMMENT: Why are these values set to an empty string?
+      // After the search results page has been initialized, these lines of code set the search criteria back to blank.
       event.target.title.value = '';
       event.target.author.value = '';
       event.target.isbn.value = '';
@@ -106,23 +110,28 @@ var app = app || {};
   }
 
   // COMMENT: What is the purpose of this method?
+  // Purpose of this method is to initialize the search results page and populate it with results from the search form.
   bookView.initSearchResultsPage = function() {
     resetView();
     $('.search-results').show();
     $('#search-list').empty();
 
     // COMMENT: Explain how the .map() method is being used below.
+    //The map method is displaying the results of the search by appending the results, while providing a link with each to save them to the main list.
     module.Book.all.map(book => $('#search-list').append(book.toHtml()));
     $('.detail-button a').text('Add to list').attr('href', '/');
     $('.detail-button').on('click', function(e) {
       // COMMENT: Explain the following line of code.
+      //This line of code invokes the Book.findOne method, passing the ISBN through as the argument.
       module.Book.findOne($(this).parent().parent().parent().data('bookid'))
     });
   }
 
   // COMMENT: Explain the following line of code. 
+  // This line of code enables the global variable app to be passed through because module was set as a parameter at the top of this IIFE
   module.bookView = bookView;
   
-  // COMMENT: Explain the following line of code. 
+  // COMMENT: Explain the following line of code.
+  //This line of code closes the IFFE and calls it using the parameter of "app"
 })(app)
 
